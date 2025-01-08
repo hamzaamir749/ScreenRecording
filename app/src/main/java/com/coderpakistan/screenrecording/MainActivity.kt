32b05bb866db6c5d564738110b9b5a8dfd787484
hamzaamir749 <hamzaamir749@gmail.com>
2025-01-08 19:53:39 +0000

    Initial commit

---
+++ app/src/main/java/com/coderpakistan/screenrecording/MainActivity.kt
@@ -0,0 +1,149 @@
+package com.coderpakistan.screenrecording
+
+import android.Manifest
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.media.projection.MediaProjectionManager
+import android.os.Build
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.content.ContextCompat
+import androidx.core.content.getSystemService
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import com.coderpakistan.screenrecording.ScreenRecordingService.Companion.KEY_RECORDING_CONFIG
+import com.coderpakistan.screenrecording.ScreenRecordingService.Companion.START_RECORDING
+import com.coderpakistan.screenrecording.ScreenRecordingService.Companion.STOP_RECORDING
+import com.coderpakistan.screenrecording.ui.theme.CoralRed
+import com.coderpakistan.screenrecording.ui.theme.MintGreen
+import com.coderpakistan.screenrecording.ui.theme.ScreenRecordingTheme
+
+class MainActivity : ComponentActivity() {
+    private val mediaProjectionManager by lazy {
+        getSystemService<MediaProjectionManager>()!!
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            ScreenRecordingTheme {
+                val isServiceRunning by ScreenRecordingService.isServiceRunning.collectAsStateWithLifecycle()
+
+
+
+                var hasNotificationPermission by remember {
+                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+                        mutableStateOf(
+                            ContextCompat.checkSelfPermission(
+                                applicationContext,
+                                Manifest.permission.POST_NOTIFICATIONS
+                            ) == PackageManager.PERMISSION_GRANTED
+                        )
+                    } else mutableStateOf(true)
+                }
+
+                val screenRecordLauncher = rememberLauncherForActivityResult(
+                    contract = ActivityResultContracts.StartActivityForResult()
+                ) { result ->
+                    val intent = result.data ?: return@rememberLauncherForActivityResult
+                    val config = ScreenRecordConfig(resultCode = result.resultCode, data = intent)
+                    val serviceIntent =
+                        Intent(applicationContext, ScreenRecordingService::class.java).apply {
+                            action = START_RECORDING
+                            putExtra(KEY_RECORDING_CONFIG, config)
+                        }
+                    startForegroundService(
+                        serviceIntent
+                    )
+                }
+                val permissionLauncher = rememberLauncherForActivityResult(
+                    contract = ActivityResultContracts.RequestPermission()
+                ) { isGranted ->
+                    hasNotificationPermission = isGranted
+                    if (hasNotificationPermission && !isServiceRunning) {
+                        screenRecordLauncher.launch(
+                            mediaProjectionManager.createScreenCaptureIntent()
+                        )
+                    }
+                }
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Box(
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .padding(innerPadding),
+                        contentAlignment = Alignment.Center
+                    ) {
+                        Button(
+                            onClick = {
+                                if (!hasNotificationPermission && Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+                                    permissionLauncher.launch(Manifest.permission.POST_NOTIFICATIONS)
+                                } else {
+                                    if (isServiceRunning) {
+                                        Intent(
+                                            applicationContext,
+                                            ScreenRecordingService::class.java
+                                        ).also {
+                                            it.action = STOP_RECORDING
+                                            startForegroundService(it)
+                                        }
+                                    } else {
+                                        screenRecordLauncher.launch(
+                                            mediaProjectionManager.createScreenCaptureIntent()
+                                        )
+                                    }
+                                }
+                            }, colors = ButtonDefaults.buttonColors(
+                                containerColor = if (isServiceRunning) {
+                                    CoralRed
+                                } else {
+                                    MintGreen
+                                }
+                            )
+                        ) {
+                            Text(
+                                text = if (isServiceRunning) "Stop recording" else "Start recording",
+                                color = Color.Black
+                            )
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    ScreenRecordingTheme {
+        Greeting("Android")
+    }
+}
